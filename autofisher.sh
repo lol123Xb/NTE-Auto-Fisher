f51aa268ac71e6e7e70d86aa2bcaae75aa37945c
Felix <herobrine.raw@gmail.com>
2026-05-11 08:31:20 +0000

    Completed script
    Had issues trying to get the script to open the map or team menu in game but turns out, making it tap where the chat button is works 10x better

---
+++ autofisher.sh
@@ -0,0 +1,41 @@
+#If you found this script to be useful, cool and awesome (like me 😎) 
+#please consider subscribing to my youtube
+#https://www.youtube.com/@trikanoidyt
+
+#!/bin/bash
+while true; do
+	sleep 4
+	xdotool key f
+	sleep 2 
+	xdotool mousemove 1631 948 
+	sleep 1 
+	xdotool click 1 
+	sleep 2 
+	xdotool key f 
+	sleep 8 
+	xdotool key f 
+	sleep 0.25
+	xdotool mousemove 72 988 
+	sleep 0.25
+	xdotool click 1 
+	sleep 0.25
+	xdotool click 1
+	sleep 0.25
+	xdotool click 1 
+	sleep 0.25
+	xdotool click 1 
+	sleep 0.25
+	xdotool click 1 
+	sleep 0.25
+	xdotool click 1 
+	sleep 0.25
+	xdotool click 1
+	sleep 10
+	xdotool mousemove 1631 948 
+	sleep 1 
+	xdotool click 1 
+	sleep 1
+	xdotool key 0xff1b
+	sleep 1
+	xdotool key 0xff1b
+done
